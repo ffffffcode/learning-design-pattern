58e647ceae0c3294e90f1424060ed31846d5f57e
AaronFae <AaronFae@163.com>
2018-01-27 03:44:01 +0800

    更新对象适配器

--- src/ex/aaronfae/pattern/adapter/ThreeForTwoAdapter.java
+++ src/ex/aaronfae/pattern/adapter/ThreeForTwoAdapter.java
@@ -1,23 +1,25 @@
 package ex.aaronfae.pattern.adapter;
 
 /**
- * 适配器类
+ * 适配器类（对象适配器）
  * 
- * 实现三相插头转二相插头，以便让三相插头插入二相插座
+ * 实现了三相插头接口
+ * 
+ * 用于三相插头转二相插头
  * 
  * @author AaronFae
  *
  */
 public class ThreeForTwoAdapter implements ThreePhasePlug {
 
-	// 适配器要拥有二相插座，才能让二相插座为其供电
+	// 适配器要拥有二相插座，才能让二相插座为三相插头供电
 	private TwoPhaseSocket twoPhaseSocket;
 
 	public ThreeForTwoAdapter(TwoPhaseSocket socket) {
 		this.twoPhaseSocket = socket;
 	}
 
-	// 具体实现了三相插头的取电行为：用适配器，就可让二相插座为三相插头供电
+	// 具体实现了三相插头的取电行为：让二相插座为三相插头供电
 	@Override
 	public void getPower() {
 		System.out.println("经过转换");
